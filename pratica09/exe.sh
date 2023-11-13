360c9f5387e54a96515cfb9ac1f53dee21cf4316
Marcos Daniel <marcosnumber11@gmail.com>
2023-11-13 09:48:42 -0300

    Atividade pratica 09

---
+++ pratica09/exe.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+./pa9.out -z 98 -s 16 -a s
+
+./pa9.out -z 98 -s 16 -a i
+
+./pa9.out -z 98 -s 16 -a q
+
+./pa9.out -z 98 -s 16 -a q3
+
+./pa9.out -z 98 -s 16 -a qi
+
+./pa9.out -z 98 -s 16 -a q3i
+
+./pa9.out -z 98 -s 16 -a h
+
+./pa9.out -z 98 -s 16 -a rs
+
+./pa9.out -z 473 -s 2 -a s
+
+./pa9.out -z 473 -s 2 -a i
+
+./pa9.out -z 473 -s 2 -a q
+
+./pa9.out -z 473 -s 2 -a q3
+
+./pa9.out -z 473 -s 2 -a qi
+
+./pa9.out -z 473 -s 2 -a q3i
+
+./pa9.out -z 473 -s 2 -a h
+
+./pa9.out -z 473 -s 2 -a rs
+
+./pa9.out -z 8721 -s 44 -a s
+
+./pa9.out -z 8721 -s 44 -a i
+
+./pa9.out -z 8721 -s 44 -a q
+
+./pa9.out -z 8721 -s 44 -a q3
+
+./pa9.out -z 8721 -s 44 -a qi
+
+./pa9.out -z 8721 -s 44 -a q3i
+
+./pa9.out -z 8721 -s 44 -a h
+
+./pa9.out -z 8721 -s 44 -a rs
+
+./pa9.out -z 34275 -s 43 -a s
+
+./pa9.out -z 34275 -s 43 -a i
+
+./pa9.out -z 34275 -s 43 -a q
+
+./pa9.out -z 34275 -s 43 -a q3
+
+./pa9.out -z 34275 -s 43 -a qi
+
+./pa9.out -z 34275 -s 43 -a q3i
+
+./pa9.out -z 34275 -s 43 -a h
+
+./pa9.out -z 34275 -s 43 -a rs
+
+./pa9.out -z 73735 -s 13 -a s
+
+./pa9.out -z 73735 -s 13 -a i
+
+./pa9.out -z 73735 -s 13 -a q
+
+./pa9.out -z 73735 -s 13 -a q3
+
+./pa9.out -z 73735 -s 13 -a qi
+
+./pa9.out -z 73735 -s 13 -a q3i
+
+./pa9.out -z 73735 -s 13 -a h
+
+./pa9.out -z 73735 -s 13 -a rs
